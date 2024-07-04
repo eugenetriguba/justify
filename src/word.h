9676632882ed7cf205dc2ac6b89175ed50a3ec49
Eugene Triguba <eugenetriguba@gmail.com>
2024-07-04 10:56:40 -0400

    fix doc

--- src/word.h
+++ src/word.h
@@ -6,9 +6,9 @@
 /**
  * Read in a word from a file up to size length.
  *
- * @param fp[in] The file to read a word from.
- * @param word[out] The word that was read as a null-terminated string.
- * @param length[in] The length of the word to read in.
+ * @param[in] fp The file to read a word from.
+ * @param[out] word The word that was read as a null-terminated string.
+ * @param[in] length The length of the word to read in.
  *
  * @note `length` should be the number of characters you'd
  *       like to read in plus one to take into account the
